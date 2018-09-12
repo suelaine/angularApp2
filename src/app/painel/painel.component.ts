3e0ab9aa51c4aa50e2a76e4a9c0fc4ed23191ea9
Suelaine Andrade <suelaine.andrade@gmail.com>
2018-09-12 18:15:29 -0300

    62. App1 - Verificando resposta parte 1

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -10,10 +10,20 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase: '
+  public resposta: string
 
   constructor() {console.log(this.frases) }
 
   ngOnInit() {
+  }
+
+  public atualizaResposta(resposta: Event):void{
+    this.resposta = (<HTMLInputElement>resposta.target).value
+    console.log(this.resposta)
+  }
+
+  public verificarResposta(): void{
+    console.log('Verificar resposta: ',this.resposta)
   }
 
 }
